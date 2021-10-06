5f6ddb6461f6824b66d08d835bb0e5984f030e8d
Omer Tuchfeld <otuchfel@redhat.com>
2021-10-06 15:04:55 +0200

    work

--- launch.sh
+++ launch.sh
@@ -8,14 +8,14 @@ pushd agent
 git reset --hard origin/master
 git pull
 git apply ../agent-patches/*
-make -o unit-test build-image
-podman tag quay.io/ocpmetal/assisted-installer-agent:latest quay.io/otuchfel/assisted-installer-agent:swarm
-podman push quay.io/otuchfel/assisted-installer-agent:swarm
+sudo make -o unit-test build-image
+sudo podman tag quay.io/ocpmetal/assisted-installer-agent:latest quay.io/otuchfel/assisted-installer-agent:swarm
+sudo podman push quay.io/otuchfel/assisted-installer-agent:swarm
 git reset --hard origin/master
 popd
 
 export SERVICE_ENDPOINT=http://192.168.39.73:31442
-export INFRA_ENV_ID=5daa967c-aa76-43db-ad10-607098dee4ae
+export INFRA_ENV_ID=b7b9f297-0475-4dcb-b3ab-338b4f5ce383
 
 export IGNITION=$(curl -s "${SERVICE_ENDPOINT}/api/assisted-install/v2/infra-envs/${INFRA_ENV_ID}/downloads/files?file_name=discovery.ign")
 export COPY_CMD=$(<<< $IGNITION jq '.systemd.units[].contents' -r | rg 'ExecStart=/usr/local/bin/agent' | cut -d'=' -f2-)
