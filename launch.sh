89faa68f79352937c6089657be33a86531bd03fe
Your Name <you@example.com>
2021-10-08 14:27:44 +0200

    Stuff

--- launch.sh
+++ launch.sh
@@ -15,12 +15,13 @@ git reset --hard origin/master
 popd
 
 export SERVICE_ENDPOINT=http://192.168.39.73:31442
-export INFRA_ENV_ID=b7b9f297-0475-4dcb-b3ab-338b4f5ce383
+export INFRA_ENV_ID=4f5b713a-d897-41a1-b5ee-d0a8579b22a2
 
 export IGNITION=$(curl -s "${SERVICE_ENDPOINT}/api/assisted-install/v2/infra-envs/${INFRA_ENV_ID}/downloads/files?file_name=discovery.ign")
 export COPY_CMD=$(<<< $IGNITION jq '.systemd.units[].contents' -r | rg 'ExecStart=/usr/local/bin/agent' | cut -d'=' -f2-)
 export AGENT_CMD=$(<<< $IGNITION jq '.systemd.units[].contents' -r | rg "podman run" | cut -d'=' -f2-)
 
+echo $COPY_CMD
 echo $AGENT_CMD
 
 sudo $COPY_CMD
