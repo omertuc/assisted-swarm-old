7523dacf21e64458edbc29359e4cc0ba0bed30e8
Your Name <you@example.com>
2021-10-08 18:25:51 +0200

    fixes

--- launch.sh
+++ launch.sh
@@ -4,14 +4,14 @@ set -euo pipefail
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-# pushd agent
-# git fetch -a
-# git reset --hard origin/master
-# git apply ../agent-patches/*
-# sudo make -o unit-test build-image
-# sudo podman push quay.io/otuchfel/assisted-installer-agent:swarm
-# git reset --hard origin/master
-# popd
+pushd agent
+git fetch -a
+git reset --hard origin/master
+git apply ../agent-patches/*
+sudo make -o unit-test build-image
+sudo podman push quay.io/otuchfel/assisted-installer-agent:swarm
+git reset --hard origin/master
+popd
 
 export SERVICE_ENDPOINT=http://192.168.39.73:31442
 export INFRA_ENV_ID=4f5b713a-d897-41a1-b5ee-d0a8579b22a2
@@ -22,4 +22,5 @@ export COPY_CMD=$(<<< $IGNITION jq '.systemd.units[].contents' -r | rg "podman r
 
 set -x 
 sudo $COPY_CMD
+
 sudo $(echo $AGENT_CMD | sed -e "s/--infra-env-id/--host-id $(uuid) --infra-env-id/")
