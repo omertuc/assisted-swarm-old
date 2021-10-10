d2f1ae1519cc15e408b0e331fd9748f0d4450226
Your Name <you@example.com>
2021-10-10 19:24:48 +0200

    Stuff

--- launch_all.sh
+++ launch_all.sh
@@ -8,9 +8,17 @@ export SERVICE_ENDPOINT=http://192.168.39.239:30478
 
 arbitrary_infraenv=$(./list_infraenvs.sh | head -1)
 
+PULL_SECRET=$(oc get secret -n swarm-1 swarm-1 -ojson | jq '.data.".dockerconfigjson"' -r | base64 -d)
+sudo mkdir -p /root/.docker
+sudo tee /root/.docker/config >/dev/null <<< $PULL_SECRET
+
+export PULL_SECRET=$(curl -s "${SERVICE_ENDPOINT}/api/assisted-install/v2/infra-envs/${arbitrary_infraenv}/downloads/files?file_name=discovery.ign")
 export IGNITION=$(curl -s "${SERVICE_ENDPOINT}/api/assisted-install/v2/infra-envs/${arbitrary_infraenv}/downloads/files?file_name=discovery.ign")
 export COPY_CMD=$(<<< $IGNITION jq '.systemd.units[].contents' -r | rg "podman run" | cut -d'=' -f2-)
 sudo $COPY_CMD
+
+sudo killall agent || true
+sudo podman ps -q | xargs sudo podman kill
 
 for infra_env in $(./list_infraenvs.sh); do 
     INFRA_ENV_ID=${infra_env} ./launch.sh &
