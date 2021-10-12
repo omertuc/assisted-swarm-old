5c0a857d56a4c6cbb7c63e9821f1562466e68dcf
Your Name <you@example.com>
2021-10-12 18:56:42 +0200

    Various fixes

--- launch_all.sh
+++ launch_all.sh
@@ -22,9 +22,13 @@ sudo touch /opt/openshift/.bootkube.done
 export PULL_SECRET=$(curl -k -s "${SERVICE_ENDPOINT}/api/assisted-install/v2/infra-envs/${arbitrary_infraenv}/downloads/files?file_name=discovery.ign")
 export IGNITION=$(curl -k -s "${SERVICE_ENDPOINT}/api/assisted-install/v2/infra-envs/${arbitrary_infraenv}/downloads/files?file_name=discovery.ign")
 
-if [[ $IGNITION == "" ]]; then
-	echo "Failed to fetch ignition file"
-	exit 1
+CA_CERT_PATH="/etc/assisted-service/service-ca-cert.crt"
+sudo mkdir -p $(dirname ${CA_CERT_PATH})
+jq '.storage.files[] | select(.path == "'$CA_CERT_PATH'").contents.source' -r <<< $IGNITION | cut -d',' -f2- | base64 -d | sudo tee $CA_CERT_PATH > /dev/null
+
+if [[ "$IGNITION" == "" ]]; then
+  echo "Failed to fetch ignition file"
+  exit 1
 fi
 
 export COPY_CMD=$(<<< $IGNITION jq '.systemd.units[].contents' -r | rg "podman run" | cut -d'=' -f2-)
