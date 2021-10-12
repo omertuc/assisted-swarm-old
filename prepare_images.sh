4e92350e9abb2b269852bef65990522d4dc222b4
Omer Tuchfeld <otuchfel@redhat.com>
2021-10-12 17:12:13 +0200

    OCM

--- prepare_images.sh
+++ prepare_images.sh
@@ -8,21 +8,21 @@ echo "Pushing to quay user ${QUAY_ACCOUNT}"
 
 pushd agent
 git fetch -a
-git reset --hard origin/master
+git reset --hard origin/release-ocm-2.4
 git apply ../agent-patches/*.patch
 make -o unit-test build-image
 podman push quay.io/${QUAY_ACCOUNT}/assisted-installer-agent:swarm
-git reset --hard origin/master
+git reset --hard origin/release-ocm-2.4
 popd
 
 pushd installer
 git fetch -a
-git reset --hard origin/replace
+git reset --hard origin/release-ocm-2.4
 git apply ../installer-patches/*.patch
 make installer-image
 make controller-image
 podman push quay.io/${QUAY_ACCOUNT}/assisted-installer:swarm
 podman push quay.io/${QUAY_ACCOUNT}/assisted-installer-controller:swarm
-git reset --hard origin/replace
+git reset --hard origin/release-ocm-2.4
 popd
 
