380f710a2748cdac66b1a07547074e5b51df681f
Omer Tuchfeld <otuchfel@redhat.com>
2021-11-19 19:22:39 +0100

    Separate taskpools for clusters and agents

--- main.py
+++ main.py
@@ -32,31 +32,33 @@ def main(max_concurrent, test_plan, service_config):
 
     pull_secret, service_config, test_plan = load_config(service_config, test_plan)
 
-    with TaskPool(max_workers=max_concurrent) as taskpool:
-        swarm = Swarm(
-            pull_secret=pull_secret,
-            service_url=service_config["service_endpoint"],
-            ssh_pub_key=service_config["ssh_pub_key"],
-        )
+    with TaskPool(max_workers=max_concurrent) as agents_taskpool:
+        with TaskPool(max_workers=max_concurrent) as clusters_taskpool:
+            swarm = Swarm(
+                pull_secret=pull_secret,
+                service_url=service_config["service_endpoint"],
+                ssh_pub_key=service_config["ssh_pub_key"],
+            )
 
-        swarm.start()
+            swarm.start()
 
-        execute_plan(taskpool, test_plan, swarm)
+            execute_plan(agents_taskpool, clusters_taskpool, test_plan, swarm)
 
     swarm.logging.info(f"All clusters finished, exiting")
     swarm.finalize()
 
 
-def execute_plan(taskpool, test_plan, swarm):
+def execute_plan(agents_taskpool, clusters_taskpool, test_plan, swarm):
     clusters = [(c["single_node"], c["num_workers"]) for c in test_plan["clusters"] for _ in range(c["amount"])]
 
     if test_plan.get("shuffle", False):
         shuffle(clusters)
 
     for cluster_index, (single_node, num_workers) in enumerate(clusters):
-        taskpool.submit(swarm.launch_cluster, cluster_index, taskpool, single_node, num_workers)
+        clusters_taskpool.submit(swarm.launch_cluster, cluster_index, agents_taskpool, single_node, num_workers)
 
-    taskpool.wait()
+    clusters_taskpool.wait()
+    agents_taskpool.wait()
 
 
 if __name__ == "__main__":
